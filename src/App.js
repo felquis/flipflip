6eedbca8e8d2fd77f379925c6f5bc7978ce32120
Felquis Gimenes <frgformenton@gmail.com>
2017-01-25 17:32:25 -0200

    Save Flips

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {DeviceOrientation} from 'react-event-components'
+import firebase from 'firebase'
 import {
   LineChart,
   XAxis,
@@ -12,6 +13,11 @@ import {
   Line,
   ResponsiveContainer
 } from 'recharts'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAX6HpGRqFpAU5blvJinLUKsOd8f2NjLOI',
+  databaseURL: 'https://flipflip-7cfe3.firebaseio.com/',
+})
 
 class App extends Component {
   constructor () {
@@ -25,19 +31,51 @@ class App extends Component {
     this.toggleRunning = this.toggleRunning.bind(this)
   }
 
+  saveFlip (orientation) {
+     const flip = {
+       orientation
+     }
+
+     const newFlip = firebase.database().ref().child('flips').push().key
+     const updates = {}
+
+     updates['/flips/' + newFlip] = flip
+
+     console.log(flip)
+
+     return firebase.database().ref().update(updates)
+  }
+
   handleDeviceOrientation = (orientation) => {
     if (!this.state.running) return
 
-    this.setState({ orientation: [...this.state.orientation, orientation] })
+    const { alpha, gamma, absolute, timeStamp, type } = orientation
+
+    const newOrientation = {
+      alpha, gamma, absolute, timeStamp, type
+    }
+
+    this.setState({ orientation: [...this.state.orientation, newOrientation] })
   }
 
   toggleRunning () {
-    if (!this.state.running) {
+    if (this.state.running) {
+      this.saveFlip(this.state.orientation)
+    } else {
       this.setState({ orientation: [] })
     }
 
     this.setState({
       running: !this.state.running
+    })
+  }
+
+  componentDidMount() {
+    const db = firebase.database()
+    let flips = db.ref('flips')
+
+    this.setState({
+      ...this.state, flips: flips
     })
   }
 
