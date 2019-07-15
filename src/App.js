be02261a5244c01d3d5051dbf8108ba7674e5315
felquis <frgformenton@gmail.com>
2019-07-14 23:52:45 -0300

    Remove firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
 import {DeviceOrientation} from 'react-event-components'
-import firebase from 'firebase'
 import {
   LineChart,
   XAxis,
@@ -11,16 +10,14 @@ import {
   ResponsiveContainer
 } from 'recharts'
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyAX6HpGRqFpAU5blvJinLUKsOd8f2NjLOI',
-  databaseURL: 'https://flipflip-7cfe3.firebaseio.com/',
-})
+let orientationHistory = []
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      orientation: [],
+      snapShot: [],
+      // orientation: [],
       running: false,
       flipCount: 0,
     }
@@ -29,107 +26,71 @@ class App extends Component {
     this.toggleRunning = this.toggleRunning.bind(this)
   }
 
-  saveFlip (orientation) {
-    const userAgent = navigator.userAgent
+  componentDidMount() {
+    this.setState({
+      running: true,
+    })
 
-    const flip = {
-      orientation,
-      fingerprint: { userAgent },
-      created_at: firebase.database.ServerValue.TIMESTAMP
-    }
+    window.addEventListener('deviceorientation', this.handleDeviceOrientation, true)
+    this.setSnapShot()
+  }
 
-    const newFlip = firebase.database().ref().child('flips').push().key
+  setSnapShot() {
+    this.timer = setTimeout(() => this.setSnapShot(), 1000)
 
-    const updates = {}
-    updates[`/flips/${this.state.uid}/${newFlip}`] = flip
-    firebase.database().ref().update(updates)
+    this.setState({
+      snapShot: orientationHistory.slice(0, orientationHistory.length - 1)
+    })
   }
 
   handleDeviceOrientation (orientation) {
-    if (!this.state.running) return
+    // alert('b')
+    // alert('a')
+    // if (!this.state.running) return
 
     const { alpha, gamma, absolute, timeStamp, type, beta } = orientation
 
     const newOrientation = {
-      alpha, gamma, absolute, timeStamp, type, beta
+      alpha, gamma, absolute, time: timeStamp, type, beta
     }
 
-    this.setState({ orientation: [...this.state.orientation, newOrientation] })
+    const total = orientationHistory.length
+        
+    if (total > 200) {
+      orientationHistory.shift()
+    }
+
+    orientationHistory.push(newOrientation)
   }
 
   toggleRunning () {
-    if (this.state.running) {
-      this.saveFlip(this.state.orientation)
-    } else {
-      this.setState({ orientation: [] })
-    }
-
     this.setState({
+      orientation: [],
       running: !this.state.running
     })
   }
 
-  getFlipPoints() {
-    if (!this.state.uid) return
-
-    firebase.database().ref(`flips/${this.state.uid}`).on('child_added', (flips) => {
-      this.setState({
-        ...this.state, flipCount: this.state.flipCount + 1
-      })
-    })
-  }
-
-  componentDidMount() {
-    let flips = firebase.database().ref('flips')
-
-    this.setState({
-      ...this.state, flips: flips
-    })
-
-    firebase.auth().onAuthStateChanged((user) => {
-      // User is signed in.
-      if (user) {
-        const uid = user.uid
-
-        this.setState({
-          ...this.state, uid: uid
-        })
-
-        this.getFlipPoints()
-
-        return
-      }
-
-      this.setState({
-        ...this.state, uid: false
-      })
-
-      // User is signed out, so create a new user
-      firebase.auth().signInAnonymously().catch((error) => {
-        const { code, message } = error
-
-        alert(`Error While Saving Anonymous ID: ${message} ${code}`)
-      })
-    })
-  }
-
   render() {
-    const {running, orientation} = this.state
+    const { snapShot } = this.state
 
     return (
       <div className="App">
-        <DeviceOrientation do={this.handleDeviceOrientation} />
+        {/* <DeviceOrientation do={this.handleDeviceOrientation} /> */}
 
         <footer className="App-footer">
-          {this.state.flipCount} flips points
-          <button className="App-button" onClick={this.toggleRunning}>{running ? 'Stop Recording': 'Start New Recording'}</button>
+          {snapShot.length}
+          {/* <button className="App-button" onClick={this.toggleRunning}>
+            {running? 'stop' : 'start'}
+          </button> */}
         </footer>
 
-        {!running && (<ResponsiveContainer height="90%" width="100%">
+        {/* <TimeSeries chartData={snapShot} /> */}
+
+        <ResponsiveContainer height="90%" width="100%">
           <LineChart
             width={730}
             height={250}
-            data={orientation}
+            data={snapShot}
             margin={{ top: 5, right: 0, left: 0, bottom: 5 }}>
             <XAxis dataKey="x" />
             <YAxis domain={[-100, 100]} />
@@ -138,7 +99,7 @@ class App extends Component {
             <Line dataKey="beta" stroke="green" animationDuration={0} dot={false}/>
             <Line dataKey="gamma" stroke="red" animationDuration={0} dot={false}/>
           </LineChart>
-        </ResponsiveContainer>)}
+        </ResponsiveContainer>
       </div>
     )
   }
